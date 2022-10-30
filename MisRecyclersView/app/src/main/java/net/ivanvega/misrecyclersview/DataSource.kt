edaa2351f0588eb6f931bebd3cb766132821885a
Gabriel Arreola <auro.prz1809@gmail.com>
2022-10-30 09:23:34 -0600

    Tarea completada

--- MisRecyclersView/app/src/main/java/net/ivanvega/misrecyclersview/DataSource.kt
+++
@@ -1,9 +0,0 @@
-package net.ivanvega.misrecyclersview
-
-import net.ivanvega.misrecyclersview.data.Flower
-
-object DataSource {
-    val lsFlower =  mutableListOf<Flower>()
-
-
-}
--- MisRecyclersView/app/src/main/java/net/ivanvega/misrecyclersview/data/Flower.kt
+++
@@ -1,8 +0,0 @@
-package net.ivanvega.misrecyclersview.data
-
-data class Flower(
-    val id: Long,
-    val name: String,
-    val image: Int?,
-    val description: String
-)
